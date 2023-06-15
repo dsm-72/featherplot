288f0a455521ca3ecd4850b5942a150c0c3b948a
dsm-72 <sumner.magruder@yale.edu>
2023-06-15 09:06:38 -0400

    slider ogod

--- src/lib/Forms/Slider.svelte
+++ src/lib/Forms/Slider.svelte
@@ -8,100 +8,137 @@
     // import Tooltip from '@fouita/tooltip';
     import { onMount } from 'svelte/internal';
     import { createEventDispatcher } from 'svelte';
+
     const dispatch = createEventDispatcher();
-    let cls = '';
-    export { cls as class };
 
-    export let min = 0;
-    export let max = 100;
-    export let value = [25, 64];
+    export let min:number = 0;
+    export let max:number = 100;
+    export let value:[number, number] = [25, 64];
 
-    export let color = 'indigo-600';
-    export let tooltip = false;
-  
+    export let color:string = 'bg-sky-500';
+    
+    export let tooltip:boolean= false;
+
+    let tooltipMin = false;
+    let tooltipMax = false;
+    
+    let pointer: HTMLElement | null = null;
+    let container: HTMLElement
+    let pointerMin: HTMLElement
+    let pointerMax: HTMLElement
+    let progressBar: HTMLElement
+    // let nodeOffsetLeft:number
+    $: nodeOffsetLeft = container?.offsetLeft
+
     
   
-    let current_pointer = null;
-  
-    function setValue(val) {
-        if (Array.isArray(value)) {
-            value[current_pointer == leftPointer ? 0 : 1] = val;
-        } else {
-            value = val;
-        }
+    function isMouseEvent(event: MouseEvent | TouchEvent): event is MouseEvent {
+        return (event instanceof MouseEvent)       
+    }
+    function isTouchEvent(event: MouseEvent | TouchEvent): event is TouchEvent {
+        return (event instanceof TouchEvent)       
+    }
+    function getInteractiveEventWidth(event: MouseEvent | TouchEvent) {
+        if (isMouseEvent(event)) return event.clientX        
+        if (isTouchEvent(event)) return event.touches[0].clientX;
+        return 0
+    }
+
+    function updateValue(val:number) {
+        let which = pointer === pointerMin ? 0 : 1
+        let other = which === 0 ? 1 : 0
+        if (which === 0 && val > value[1]) {which = 1; return}
+        if (which === 1 && val < value[0]) {which = 0; return}
+        
+        tooltipMin = which === 0
+        tooltipMax = which === 1
+        
+        value[which] = val
         dispatch('change', value)
     }
-  
-    function movePointer(e) {
-      if (!current_pointer) return false;
-      let cx = e.clientX || e.touches[0].clientX;
-      let diff = cx - nodeOffsetLeft; // calculate percentage
-      let per = (diff * 100) / width;
-      per = per < 0 ? 0 : per > 100 ? 100 : per;
-      setValue(parseInt((per * (max - min)) / 100) + min);
-    }
-  
-    let nodeOffsetLeft = null;
-    let leftPointer = null;
-    let rightPointer = null;
-    let progress_bar = null;
-  
-    let display_tooltip1 = false;
-    let display_tooltip2 = false;
-  
-    function setOffsetLeft(node:HTMLElement) {
-      nodeOffsetLeft = node.offsetLeft;
-      console.log(offWidth, node.offsetLeft)
-    }
-  
-    function setLeftPointer(node:HTMLElement) {
-      leftPointer = node;
-    }
-  
-    function setRightPointer(node:HTMLElement) {
-      rightPointer = node;
-    }
-  
 
-    function progressBar(node:HTMLElement) {
-      progress_bar = node;
+    function movePointer(event:MouseEvent | TouchEvent) {        
+        if (pointer === null) return false;
+        let clientX = getInteractiveEventWidth(event)
+
+        // calculate percentage
+        let diff = clientX - nodeOffsetLeft; 
+        let prec = (diff * 100) / width;     
+
+        const val = calcPerc(prec)
+        const adj = val + min
+        updateValue(adj);
     }
+    
+    
+  
+    // function setOffsetLeft(node:HTMLElement) {
+    //   nodeOffsetLeft = node.offsetLeft;
+    // }
+    // function setPointerMin(node:HTMLElement) {
+    //   pointerMin = node;
+    // }
+    // function setRightPointer(node:HTMLElement) {
+    //   pointerMax = node;
+    // }
+    // function setProgressBar(node:HTMLElement) {
+    //   progressBar = node;
+    // }
   
     function mouseUp() {
-      current_pointer = null;
-      display_tooltip1 = tooltip == 'hover' ? false : tooltip;
-      display_tooltip2 = display_tooltip1;
+      pointer = null;
+      tooltipMin = false
+      tooltipMax = false;
     }
   
-    $: if (progress_bar && leftPointer) {
+    function updatePointerStyle(pointer:HTMLElement, val:number) {        
+        let per = calcAdjustedPerc(forceLims(val))
+        pointer.style.left = `${per}%`
+        return per
+    }
+
+    const squashMax = (val:number) => Math.min(max, val)
+    const upliftMin = (val:number) => Math.max(min, val)
+    const forceLims = (perc:number) => squashMax(upliftMin(perc))
+    const calcPerc = (perc:number) => (forceLims(perc) * (max - min)) / 100
+    const calcAdjustedPerc = (val:number) => ((val - min) * 100) / (max - min)
+
+    $: if (progressBar && pointerMin) {
       if (Array.isArray(value)) {
-        value[0] = value[0] > min ? value[0] : min;
-        value[1] = value[1] < max ? value[1] : max;
-  
-        let per1 = ((value[0] - min) * 100) / (max - min);
-        leftPointer.style.left = `${per1}%`;
-        let per2 = ((value[1] - min) * 100) / (max - min);
-        rightPointer.style.left = `${per2}%`;
-  
-        progress_bar.style.width = `${per2 - per1}%`;
-        progress_bar.style.left = `${per1}%`;
-      } else {
-        value = value > min ? value : min;
-        value = value < max ? value : max;
-        let per = ((value - min) * 100) / (max - min);
-        leftPointer.style.left = `${per}%`;
-        progress_bar.style.width = `${per}%`;
+        value[0] = forceLims(value[0])
+        value[1] = forceLims(value[1])
+        let perMin = updatePointerStyle(pointerMin, value[0])
+        let perMax = updatePointerStyle(pointerMax, value[1])
+        progressBar.style.width = `${perMax - perMin}%`;
+        progressBar.style.left = `${perMin}%`;
       }
     }
   
-  
     $: if (tooltip == true) {
-      display_tooltip1 = true;
-      display_tooltip2 = true;
+        tooltipMin = true;
+        tooltipMax = true;
     }
   
     let width: number;
-    let offWidth: number;
+    
+    import { offset, flip, shift } from "svelte-floating-ui/dom";
+    import { createFloatingActions } from "svelte-floating-ui";
+
+    const [ floatingRefMin, floatingContentMin ] = createFloatingActions({
+        strategy: "absolute",
+        placement: "top",
+        middleware: [
+            // offset(), flip(), shift(),
+        ]
+    });
+
+    const [ floatingRefMax, floatingContentMax ] = createFloatingActions({
+        strategy: "absolute",
+        placement: "top",
+        middleware: [
+            // offset(), flip(), shift(),
+        ]
+    });
 </script>
   
 <svelte:window
@@ -113,57 +150,62 @@
 
 
 <div class={$$props.class}>
-    <div class='py-1 relative min-w-full' use:setOffsetLeft>
-        <div bind:clientWidth={width} class='h-1 -mt-px bg-gray-300 rounded-full' >
-        <div class='absolute h-1 rounded-full bg-{color} w-0' use:progressBar/>        
-        <div
-            class='absolute h-4 -mt-1 flex items-center justify-center w-4
-            rounded-full bg-white shadow border border-gray-300 -ml-2 top-0
-            cursor-leftPointer'          
-            unselectable='on'          
-            use:setLeftPointer
-            on:touchstart={() => (current_pointer = leftPointer)}
-            on:mousedown={() => (current_pointer = leftPointer)}
-            on:mouseenter={() => (display_tooltip1 = tooltip || tooltip == 'hover')}
-            on:mouseleave={() => (display_tooltip1 = (!!tooltip && !!current_pointer) || (tooltip == 'hover' ? false : tooltip))}
-        >
-          <div class='relative -mt-2 w-1'>
-            ttip 1
-            <!-- <Tooltip
-              text={(Array.isArray(indLabel) && indLabel[0]) || indLabel}
-              show={display_tooltip1} /> -->
-          </div>
-        </div>
-
-        {#if Array.isArray(value)}
-          <div
-            key='k7'
-            class='absolute h-4 -mt-1 flex items-center justify-center w-4
-            rounded-full bg-white shadow border border-gray-300 -ml-2 top-0
-            cursor-leftPointer'
-            unselectable='on'
-            onselectstart='return false;'
-            use:setRightPointer
-            on:touchstart={() => (current_pointer = rightPointer)}
-            on:mousedown={() => (current_pointer = rightPointer)}
-            on:mouseenter={() => (display_tooltip2 = tooltip || tooltip == 'hover')}
-            on:mouseleave={() => (display_tooltip2 = (!!tooltip && !!current_pointer) || (tooltip == 'hover' ? false : tooltip))}>
+    <div class='py-1 relative min-w-full select-none' bind:this={container}>
+        <div bind:clientWidth={width} class='h-1 -mt-px bg-gray-300 rounded-full'>
             
-            <div class='relative -mt-2 w-1'>
-                ttip 2              
+            <div class='absolute h-1 rounded-full {color} w-0 pointer-events-none' bind:this={progressBar}/>
+
+            <div
+                class='absolute h-4 -mt-1 flex items-center justify-center w-4
+                rounded-full bg-white shadow border border-gray-300 -ml-2 top-0
+                cursor-pointerMin'
+                unselectable='on'
+                bind:this={pointerMin}
+
+                on:touchstart={() => (pointer = pointerMin)}
+                on:mousedown={() =>  (pointer = pointerMin)}
+
+                on:mouseenter={() => (tooltipMin = tooltip)}
+                on:mouseleave={() => (tooltipMin = false)}
+                use:floatingRefMin
+            >
+                {#if tooltipMin}
+                <div class='relative -mt-2 w-1 pointer-events-none' use:floatingContentMin>
+                    {value[0]}                
+                </div>
+                {/if}
             </div>
 
-          </div>
-        {/if}
+            <div
+                class='absolute h-4 -mt-1 flex items-center justify-center w-4
+                rounded-full bg-white shadow border border-gray-300 -ml-2 top-0
+                cursor-pointerMin'
+                unselectable='on'
+                bind:this={pointerMax}
 
-        <div class='absolute text-gray-800 -ml-1 bottom-0 left-0 -mb-6'>
-            {min}
-        </div>
+                on:touchstart={() => (pointer = pointerMax)}
+                on:mousedown={() =>  (pointer = pointerMax)}
 
-        <div class='absolute text-gray-800 -mr-1 bottom-0 right-0 -mb-6'>
-            {max}
+                on:mouseenter={() => (tooltipMax = tooltip)}
+                on:mouseleave={() => (tooltipMax = false)}
+                use:floatingRefMax
+            >
+                {#if tooltipMax}
+                <div class='relative -mt-2 w-1 pointer-events-none' use:floatingContentMax>
+                    {value[1]}
+                </div>
+                {/if}
+            </div>
+
+
+            <div class='absolute text-gray-800 -ml-1 bottom-0 left-0 -mb-6'>
+                {min}
+            </div>
+
+            <div class='absolute text-gray-800 -mr-1 bottom-0 right-0 -mb-6'>
+                {max}
+            </div>
+
         </div>
-      </div>
     </div>
-  
-  </div>
+</div>
