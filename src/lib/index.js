2aadf665faeb6dd25c59542c7c9b5d47441efdc1
dsm-72 <sumner.magruder@yale.edu>
2023-06-18 08:13:11 -0400

    cleaner and ready to go

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,18 +1,27 @@
 // Reexport your entry components here
+
+import AxesSelect from "./FeatherPlot/AxesSelect.svelte";
+import AxisSelect from "./FeatherPlot/AxisSelect.svelte";
+import ChannelSelect from "./FeatherPlot/ChannelSelect.svelte";
+import ColorSelect from "./FeatherPlot/ColorSelect.svelte";
 import FeatherPlot from "./FeatherPlot/FeatherPlot.svelte";
-import ColorChannel from "./FeatherPlot/ColorChannel.svelte";
-import MaxPoints from "./FeatherPlot/MaxPoints.svelte";
+import MaxPointsSlider from "./FeatherPlot/MaxPointsSlider.svelte";
 import RangeSlider from "./FeatherPlot/RangeSlider.svelte";
-import RootChannel from "./FeatherPlot/RootChannel.svelte";
 import Slider from "./FeatherPlot/Slider.svelte";
 import ZoomCall from "./FeatherPlot/ZoomCall.svelte";
 
+import {plotStore} from "./FeatherPlot/PlotStore.ts";
+
+
 export {
+    AxesSelect,
+    AxisSelect,
+    ChannelSelect,
+    ColorSelect,
     FeatherPlot,
-    ColorChannel,
-    MaxPoints,
+    MaxPointsSlider,
     RangeSlider,
-    RootChannel,
     Slider,
     ZoomCall,
+    plotStore
 }
